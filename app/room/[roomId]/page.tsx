fb0deb3f003eb050ecc85a49e968af174be314c4
prabuddhaxdev <prabuddha1749@gmail.com>
2026-01-27 23:53:36 +0530

    Implement initial  message Ui

--- app/room/[roomId]/page.tsx
+++ app/room/[roomId]/page.tsx
@@ -70,8 +70,33 @@ export default function RoomId() {
           <span className="group-hover:animate-pulse">💣</span>
           DESTROY NOW
         </button>
-
       </header>
+
+      {/* message */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-4 scrollbar-thin"></div>
+
+      <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
+        <div className="flex gap-4">
+          <div className="flex-1 relative group">
+            <span className="absolute left-4 top-1/2 -translate-y-1/2 text-green-400 animate-pulse">
+              {"<"}
+            </span>
+
+            <input
+              autoFocus
+              type="text"
+              placeholder="Type messages..."
+              className="w-full bg-black border border-zinc-800 focus:border-zinc-700 focus:outline-none transition-colors text-zinc-100 placeholder:text-zinc-700 py-3 pl-8 pr-4 text-sm"
+            />
+          </div>
+
+          <button
+            className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold hover:text-zinc-200 transition-all disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
+          >
+            SEND
+          </button>
+        </div>
+      </div>
     </main>
   );
 }
